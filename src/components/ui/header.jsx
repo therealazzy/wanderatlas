97a020dcc4728b181c5e381882f4cbe6ac7a6474
azzy <arslanboulafrad@gmail.com>
2025-08-12 17:47:13 +0100

    styling update

--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -5,7 +5,7 @@ import { Home } from 'lucide-react'
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 w-full p-4 bg-black flex justify-between items-center">
-      <Link to="/" className="text-white hover:text-pink-300">
+      <Link to="/" className="text-white hover:text-blue-300">
         <Home size={24} />
       </Link>
     </header>
